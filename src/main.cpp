654974ded5442d331cd67bb52097ea9c0942d60a
Kracav4ik <dikama2013@yandex.ru>
2016-11-20 18:50:30 +0300

    Box2D nailed to the code

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <QApplication>
-
+#include <Box2D/Box2d.h>
 
 #include "MainWindow.h"
+
+
+b2Vec2 gravity(0.0f, -10.0f);
 
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
