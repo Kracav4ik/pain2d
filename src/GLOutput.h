cf88a8665c2fe083d4b593b075d3aba667b21860
Kracav4ik <dikama2013@yandex.ru>
2016-11-20 20:32:14 +0300

    Box2D really actually works

--- src/GLOutput.h
+++ src/GLOutput.h
@@ -2,11 +2,36 @@
 
 #include <QOpenGLWidget>
 #include <stdio.h>
+#include <QPainter>
+#include <Box2D/Box2d.h>
 
 class GLOutput : public QOpenGLWidget {
 Q_OBJECT
+private:
+    b2Body* body;
+
+protected:
+    virtual void paintGL() override {
+        QPainter painter(this);
+        painter.setPen(Qt::white);
+        if (body) {
+            const b2Vec2& p = body->GetPosition();
+            painter.drawEllipse(100+p.x*5, 100+p.y*30, 50, 50);
+        }
+    }
+
+    virtual void resizeGL(int w, int h) override {
+    }
+
+    virtual void initializeGL() override {
+    }
+
 public:
-    GLOutput(QWidget* parent) : QOpenGLWidget(parent) {
+    GLOutput(QWidget* parent) : QOpenGLWidget(parent), body(nullptr) {
+    }
+
+    void setBody(b2Body* b) {
+        body = b;
     }
 };
 
--- src/MainWindow.h
+++ src/MainWindow.h
@@ -3,16 +3,34 @@
 #include <QMainWindow>
 #include <QFontDialog>
 #include <QFileDialog>
+#include <QTimer>
+#include <Box2D/Box2d.h>
 
 #include "ui_main.h"
 
 class MainWindow : public QMainWindow, private Ui::MainWindow {
 Q_OBJECT
+private:
+    QTimer timer;
+    b2World* world;
+
 public:
-    MainWindow() {
+    MainWindow(b2Body* pBody, b2World* pWorld) : world(pWorld) {
         setupUi(this);
+        glWidget->setBody(pBody);
         show();
+        QObject::connect(&timer, SIGNAL(timeout()), this, SLOT(tick()));
+        timer.start(1);
     }
 
+public slots:
+    void tick() {
+        float32 timeStep = 0.5 / 60.0f;
+        int32 velocityIterations = 6;
+        int32 positionIterations = 2;
+
+        world->Step(timeStep, velocityIterations, positionIterations);
+        glWidget->update();
+    };
 };
 
